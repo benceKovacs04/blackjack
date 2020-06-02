dfcd7a561b7e36bed21b49d4786d72201b8161a9
Bence Kovács <kovacs.ben.p@gmail.com>
2020-06-02 11:51:23 +0200

    dealer hand is now centered, result is more visible now

--- blackjack-frontend/src/components/Player/Dealer.tsx
+++ blackjack-frontend/src/components/Player/Dealer.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import classes from './player.module.css'
 
 export default function Dealer(props: any) {
     return (
-        <div>
+        <div className={classes.Dealer}>
             <h1>{props.dealer.dealerHandValue}</h1>
-            {props.dealer.dealerHand.map((h: any) => {
-                return <img src={`http://localhost:5000/gameManager/card-image?cardId=${h.card}.png`}></img>
-            })}
+            <div>
+                {props.dealer.dealerHand.map((h: any) => {
+                    return <img src={`http://localhost:5000/gameManager/card-image?cardId=${h.card}.png`}></img>
+                })}
+            </div>
         </div>
     )
 }
